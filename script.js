0871ddfadfa6571f6daf860540354eb393355160
Her tomoe <arturmateiro9@gmail.com>
2026-02-13 01:56:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,18 +13,16 @@ function changeVolume(value) {
     music.volume = value;
 }
 
-/* Switch page + tell next page to play music2 */
 function goToLovePage() {
-    localStorage.setItem("playMusic2", "true");
     window.location.href = "love.html";
 }
 
-/* NO button safe movement */
+/* NO button runs across entire page */
 function moveButton() {
     let button = document.querySelector(".no-btn");
 
-    let maxX = window.innerWidth - button.offsetWidth - 20;
-    let maxY = window.innerHeight - button.offsetHeight - 120;
+    let maxX = window.innerWidth - button.offsetWidth;
+    let maxY = window.innerHeight - button.offsetHeight;
 
     let randomX = Math.floor(Math.random() * maxX);
     let randomY = Math.floor(Math.random() * maxY);
